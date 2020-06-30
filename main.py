c9c1f5932c4cfe40616079c34e4407a7eb74841b
ChernoBen <bneto@computacao.cua.ufmt.br>
2020-06-30 23:00:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+
+python_pages = [
+{'title':'Official Python Tutorial',
+'url':'http://docs.python.org/3/tutorial/'},
+{'title':'How to Think like a Computer Scientist',
+'url':'http://www.greenteapress.com/thinkpython/'},
+{'title':'Lean Python in 10 Minutes',
+'url':'http://www.korokithakis.net/tutorials/python/'}
+]
+
+django_pages = [
+{'title':'Official Django Tutorial',
+'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
+{'title':'Django Rocks',
+'url':'http://www.djangorocks.com'},
+{'title':'How to Tango with Django',
+'url':'http://www.tangowithdjango.com/'}
+]
+
+
+other_pages = [
+{'title':'Bottle',
+'url':'http://bottlepy.org/docs/dev'},
+{'title':'Flask',
+'url':'http://flask.pocoo.org'}
+]
+
+
+
+
+cats = {
+      'Python':{'pages':python_pages,'views':128,'likes':64},
+      'Django':{'pages':django_pages,'views':64,'likes':32},
+      'other':{'pages':other_pages,'views':32,'likes':16}
+    }
+
+for cat,cat_data in cats.items():
+  
+  print(cat,cat_data['views'],cat_data['likes'])
